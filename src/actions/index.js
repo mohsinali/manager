3882b5efb4b7c16cf6a051a755a636d272d34665
Mohsin <mohammadmohsin@gmail.com>
2019-06-08 20:13:26 +0500

    Accounts are being created.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,7 @@
-import firebase from 'firebase';
-import { EMAIL_CHANGED, PASSWORD_CHANGED } from './types';
+import firebase2 from 'firebase';
+import {
+  EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL
+} from './types';
 
 export const emailChanged = (text) => {
   return {
@@ -17,9 +19,27 @@ export const passwordChanged = (text) => {
 
 export const loginUser = ({ email, password }) => {
   return (dispatch) => {
-    firebase.auth().signInWithEmailAndPassword(email, password)
+    firebase2.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
-        dispatch({ type: 'LOGIN_USER_SUCCESS', payload: user });
+        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+      })
+      .catch(() => {
+        firebase2.auth().createUserWithEmailAndPassword(email, password)
+          .then(user => loginUserSuccess(dispatch, user))
+          .catch(() => loginUserFail(dispatch));
       });
-  }
+  };
 };
+
+const loginUserSuccess = (dispatch, user) => {
+  dispatch({
+    type: LOGIN_USER_SUCCESS,
+    payload: user
+  })
+}
+
+const loginUserFail = (dispatch) => {
+  dispatch({
+    type: LOGIN_USER_FAIL
+  });
+}
