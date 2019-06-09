4d3874a0e11dc81edb172f0d46eef9b40e714df9
Mohsin <mohammadmohsin@gmail.com>
2019-06-09 14:58:16 +0500

    Scene buckets

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,4 +1,5 @@
 import firebase2 from 'firebase';
+import { Actions } from 'react-native-router-flux';
 import {
   EMAIL_CHANGED, PASSWORD_CHANGED, LOGIN_USER_SUCCESS, LOGIN_USER_FAIL, LOGIN_USER
 } from './types';
@@ -21,12 +22,15 @@ export const loginUser = ({ email, password }) => {
 
   return (dispatch) => {
     dispatch({ type: LOGIN_USER });
+    console.log('Flag A');
 
     firebase2.auth().signInWithEmailAndPassword(email, password)
       .then(user => {
-        dispatch({ type: LOGIN_USER_SUCCESS, payload: user });
+        console.log('Flag 1');
+        loginUserSuccess(dispatch, user)
       })
       .catch(() => {
+        console.log('Inside Catch');
         firebase2.auth().createUserWithEmailAndPassword(email, password)
           .then(user => loginUserSuccess(dispatch, user))
           .catch(() => loginUserFail(dispatch));
@@ -38,7 +42,9 @@ const loginUserSuccess = (dispatch, user) => {
   dispatch({
     type: LOGIN_USER_SUCCESS,
     payload: user
-  })
+  });
+
+  Actions.main();
 }
 
 const loginUserFail = (dispatch) => {
