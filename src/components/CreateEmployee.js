c7d60d841715eab99f79adedeff48e1af6235e3e
Mohsin <mohammadmohsin@gmail.com>
2019-06-09 17:38:31 +0500

    Create employee component added

---
+++ src/components/CreateEmployee.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class CreateEmployee extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Create Employee Form</Text>
+      </View>
+    );
+  }
+};
+
+export default CreateEmployee;
